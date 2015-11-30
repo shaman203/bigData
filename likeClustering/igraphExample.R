545f59bd81a8cfaed6527f873a4802d5ad90dcce
shaman203 <matyi204@gmail.com>
2015-11-30 01:30:09 +0100

    added graph display

--- likeClustering/igraphExample.R
+++ likeClustering/igraphExample.R
@@ -13,3 +13,24 @@ write.csv(groupAssigments, file="blogGroups.csv")
 
 #par(mar=c(1,1,1,1)) #plot stupidity rectifier
 #plot(g,edge.width=E(g)$weight/2, vertex.size = 0)
+
+groupAssigments <- read.csv("blogGroups.csv")
+groupEdgeList <- cbind(groupAssigments[match(el[,1],groupAssigments[,2]),3],groupAssigments[match(el[,2],groupAssigments[,2]),3], el[,3])
+
+
+groupedEdges <- aggregate(groupEdgeList[,3], list(groupEdgeList[,1],groupEdgeList[,2]), FUN=mean)
+names(groupedEdges) <- c("V1", "V2", "weight")
+
+#groupedEdges2 <- groupedEdges[groupedEdges[,1] < 12,]
+g2 <- graph.data.frame(groupedEdges,directed = TRUE)
+
+#remove solitary edges(  degree <= 2)
+
+g2 <- simplify(g2)
+ids <- degree(g2);
+#g2 <- delete.edges(g2,E(g2)[names(ids[ids <= 2]) %--% names(ids[ids <= 2])])
+g2 <- delete.vertices(g2, V(g2)[names(ids[ids == 0])])
+
+
+
+plot(g2, edge.width=E(g2)$weight/2)
